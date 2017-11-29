7d41f4460cb3a678d582c2d4eefe6f374bb92187
MauricioZ <zelaya.mauricio@gmail.com>
2017-11-29 15:08:09 -0400

    second class

--- module1/is_prime.py
+++ module1/is_prime.py
@@ -1,19 +1,17 @@
 def is_prime(n):
-    div = 1
-    counter = 0
+    div = 2
+    if n == 1:
+        return False
     while div <= n/2:
         if n % div == 0:
-            counter += 1
-        div += 1
-        if counter > 1:
             return False
     return True
 
 
-print(is_prime(5))
-print(is_prime(4))
-print(is_prime(1))
-print(is_prime(6))
-print(is_prime(6546566668764))
-print(is_prime(668761))
+# print(is_prime(5))
+# print(is_prime(4))
+# print(is_prime(1))
+# print(is_prime(6))
+# print(is_prime(6546566668764))
+# print(is_prime(668761))
 
--- module1/position_test.py
+++ module1/position_test.py
@@ -3,7 +3,11 @@ import unittest
 class PositionTest(unittest.TestCase):
     def test_fail(self):
         self.assertEqual(1, 2, "1 is not 2")
-        self.assertTrue(False)
+
+    def test_pass(self):
+        n = 5
+        self.assertEqual(4, n)
+
 
 if __name__ == "__main__":
     unittest.main()
